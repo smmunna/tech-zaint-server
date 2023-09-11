e309c06e06d9cd21848ef1d0736c818d5b1e9874
smmunna <minhazulabedinmunna@gmail.com>
2023-09-11 23:30:29 +0600

    updated

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const userRouter = require('./router/user/userRouter')
 const coursesRouter = require('./router/courses/coursesRouter')
 
 app.use(cors())
-app.use(express.json())
+app.use(express.json({ limit: '50mb' })); //Payload size 50mb , per request it send 50mb
 
 app.use('/uploads',express.static('uploads'))
 
--- router/courses/coursesRouter.js
+++ router/courses/coursesRouter.js
@@ -1,12 +1,87 @@
 const router = require('express').Router()
+const con = require('../../db/connection');
 const courses = require('../../db/courses.json')
+const upload = require('../../components/uploadImage/uploadImage')
 
-// Axios post data to the database;
-router.post('/', (req, res) => {
-    const courses = req.body;
-    res.send(courses.courseInfo)
+// All courses getting
+router.get('/all', (req, res) => {
+    try {
+        const sql = `SELECT * FROM courses`
+        con.query(sql, (err, result) => {
+            if (err) throw err
+            res.send(result)
+        })
+    } catch (error) {
+
+    }
 })
 
+// Single course;
+router.get('/:id',(req,res)=>{
+    const id = req.params.id;
+    try {
+        const sql = `SELECT id, title, category, description, price, discount_price, rating, instructor, demo, thumbnail, total_time, content_preview FROM courses WHERE id= ?`
+        con.query(sql,[id],(err,result)=>{
+            if(err) throw err
+            res.send(result)
+        })
+    } catch (error) {
+        
+    }
+})
+
+
+// Axios post data to the database;
+router.post('/', upload.single("thumbnail"), (req, res) => {
+    if (!req.file) {
+        return res.status(400).json({ error: "No file uploaded." });
+    }
+    const courses = req.body;
+
+    const {
+        title,
+        category,
+        description,
+        price,
+        discount_price,
+        rating,
+        instructor,
+        demo,
+        total_time,
+        content_preview
+    } = courses
+
+    const thumbnail = "http://localhost:3000/" + req.file.path.replace("\\", "/");
+
+    try {
+        const sql = `INSERT INTO courses( title, category, description, price, discount_price, rating, instructor, demo, thumbnail, total_time,content_preview) 
+        VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+        con.query(sql, [
+            title,
+            category,
+            description,
+            price,
+            discount_price,
+            rating,
+            instructor,
+            demo,
+            thumbnail,
+            total_time,
+            content_preview
+        ],
+            (err, result) => {
+                if (err) throw err;
+                res.send({ status: 'ok' })
+            }
+        )
+
+    } catch (error) {
+        res.send(error.message)
+    }
+
+})
+
+// for testing purpose;
 router.get('/', (req, res) => {
     res.send(courses)
 })
